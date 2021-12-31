8e053197532652802ed45376bb5b87dc1db03588
Karthik Rao <devkarthikrao@gmail.com>
2021-12-31 16:37:55 -0500

    adds dockerrun file

--- build-scripts/post-build.sh
+++ build-scripts/post-build.sh
@@ -2,6 +2,23 @@
 
 echo "Creating build artifacts"
 
+frui_version=$IMAGE_TAG
+
+cat >Dockerrun.aws.json <<EOL
+{
+  "AWSEBDockerrunVersion": "1",
+  "Image": {
+    "Name": "877638373584.dkr.ecr.us-east-1.amazonaws.com/fr-ui:${frui_version}",
+    "Update": "true"
+  },
+  "Ports": [{
+    "ContainerPort": 80,
+    "HostPort": 80
+  }]
+}
+EOL
+
+cat Dockerrun.aws.json
 
 echo "Pushing the Docker images..."
 docker push $REPOSITORY_URI:latest
