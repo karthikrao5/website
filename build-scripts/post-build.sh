63ca951ab0a4b5dc4e7c7f8f89dfc08c5a33b61b
Karthik Rao <devkarthikrao@gmail.com>
2021-12-31 16:48:01 -0500

    adds dockerrun file

--- build-scripts/post-build.sh
+++ build-scripts/post-build.sh
@@ -2,13 +2,13 @@
 
 echo "Creating build artifacts"
 
-frui_version=$IMAGE_TAG
+image_version=$IMAGE_TAG
 
 cat >Dockerrun.aws.json <<EOL
 {
   "AWSEBDockerrunVersion": "1",
   "Image": {
-    "Name": "877638373584.dkr.ecr.us-east-1.amazonaws.com/fr-ui:${frui_version}",
+    "Name": "227046024389.dkr.ecr.us-east-1.amazonaws.com/web:${image_version}",
     "Update": "true"
   },
   "Ports": [{
